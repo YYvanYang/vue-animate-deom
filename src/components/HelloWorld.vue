1d3e646ded3049f1c82d1e5e0a76b13946007afc
Yvan Yang <yvan.yang@outlook.com>
2017-11-16 23:29:32 +0800

    update with pic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,29 +5,29 @@
   <button v-on:click="remove">Remove</button>
   <div>动画对比</div>
   <div>demo1:enter-to,leave-to</div>
-  <transition-group name="list-complete" tag="p" enter-to-class="animated bounceInRight" 
-  leave-to-class="animated zoomOutLeft">
-    <span
+  <transition-group name="list-complete" tag="p" enter-to-class="animated bounceInRight" class="pic-container"
+  leave-to-class="animated bounceOutLeft">
+    <div
       v-for="item in items"
       v-bind:key="item"
       class="list-complete-item"
     >
-      {{ item }}
-    </span>
+      <img src="../assets/1.jpg" alt="" widtd="100px" height="100px">
+    </div>
   </transition-group>
 
   <br>
   <div>demo2:enter-active,leave-active</div>
 
-  <transition-group name="list-complete" tag="p" enter-active-class="animated bounceInRight" 
+  <transition-group name="list-complete" tag="p" enter-active-class="animated bounceInRight" class="pic-container"
   leave-active-class="animated zoomOutLeft">
-    <span
+    <div
       v-for="item in items"
       v-bind:key="item"
       class="list-complete-item"
     >
-      {{ item }}
-    </span>
+      <img src="../assets/1.jpg" alt="" widtd="100px" height="100px">
+    </div>
   </transition-group>
 
   <div>demo3:enter-class</div>
@@ -71,15 +71,32 @@ export default {
       this.items = _.shuffle(this.items)
     }
   }
+  ,mounted() {
+    setInterval(() => {
+      if (this.items.length > 0) {
+        this.items.shift();
+        this.items.splice(this.items.length, 0, this.nextNum++)
+      }
+    }, 2000)
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.pic-container {
+  display: flex;
+  justify-content: center;
+
+}
+
 .list-complete-item {
-  /* transition: all 1s; */
-  display: inline-block;
-  margin-right: 10px;
+  display: block;
+  margin: 10px;
+}
+
+.list-complete-enter-active, .list-complete-leave-active {
+  transition: opacity 0.25s ease-out;
 }
 
 /* <transition-group> 组件还有一个特殊之处。
@@ -89,11 +106,18 @@ export default {
 像之前的类名一样，可以通过 name 属性来自定义前缀，
 也可以通过 move-class 属性手动设置。 */
 .list-complete-move {
-  transition: all 1s;
+  transition: all .3s linear;
+}
+
+
+.list-complete-enter, .list-complete-leave-to
+/*  for below version 2.1.8 */ {
+  opacity: 0;
 }
 
 .list-complete-leave-active {
   position: absolute;
+
 }
 
 
